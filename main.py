30bacc5d111787df5524bc6a8dda331baec4c267
sammyfranklin <sammyfranklin@yahoo.com>
2019-10-17 22:59:28 -0700

    fixed bugs and made compatible for python3

--- main.py
+++ main.py
@@ -7,8 +7,9 @@
 #
 #-------------------------------------------------------------------------------
 from __future__ import division
-from Tkinter import *
-import tkMessageBox
+from tkinter import *
+
+from tkinter import messagebox as tkMessageBox
 from PIL import Image, ImageTk
 import os
 import sys
@@ -159,7 +160,7 @@ class LabelTool():
         self.imageDir = os.path.join(r'./Images', '%s' %(self.category))
         self.imageList = glob.glob(os.path.join(self.imageDir, '*.jpg'))
         if len(self.imageList) == 0:
-            print 'No .jpg images found in the specified dir!'
+            print ('No .jpg images found in the specified dir!')
             tkMessageBox.showerror("Error!", message = "No .jpg images found in the specified dir!")
             return
 
@@ -174,7 +175,7 @@ class LabelTool():
         if not os.path.exists(self.outDir):
             os.mkdir(self.outDir)
         self.loadImage()
-        print '%d images loaded from %s' %(self.total, s)
+        print ('%d images loaded from %s' %(self.total, s))
 
     def loadImage(self):
         # load image
@@ -215,7 +216,7 @@ class LabelTool():
                 b = (float(xmin), float(xmax), float(ymin), float(ymax))
                 bb = self.convert((self.curimg_w,self.curimg_h), b)
                 f.write(str(bboxcls) + " " + " ".join([str(a) for a in bb]) + '\n')
-        print 'Image No. %d saved' %(self.cur)
+        print ('Image No. %d saved' %(self.cur))
 
 
     def mouseClick(self, event):
--- process.py
+++ process.py
@@ -1,31 +1,36 @@
-import glob, os
+import glob, os, sys
+
+if len(sys.argv) != 2:
+    print("Usage: process.py <img dir>")
+    sys.exit()
+
 
 # Current directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
+current_dir = os.path.abspath(sys.argv[1])
 
-print(current_dir)
 
-current_dir = 'Your dataset path.'
+# current_dir = 'Your dataset path.'
 
 # Directory where the data will reside, relative to 'darknet.exe'
 #path_data = './NFPAdataset/'
 
 # Percentage of images to be used for the test set
-percentage_test = 10;
+percentage_test = 10
 
 # Create and/or truncate train.txt and test.txt
 file_train = open('train.txt', 'w')
 file_test = open('test.txt', 'w')
 
 # Populate train.txt and test.txt
-counter = 1
+counter = 0
 index_test = round(100 / percentage_test)
+print(index_test)
 for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
-    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+    title, _ = os.path.splitext(os.path.basename(pathAndFilename))
 
-    if counter == index_test:
-        counter = 1
+    if counter == 0:
         file_test.write(current_dir + "/" + title + '.jpg' + "\n")
     else:
         file_train.write(current_dir + "/" + title + '.jpg' + "\n")
-        counter = counter + 1
+    
+    counter = (counter + 1) % index_test
